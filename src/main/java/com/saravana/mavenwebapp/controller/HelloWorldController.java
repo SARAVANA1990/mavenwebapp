88261bee4413f758b0a0a413309361fd9607b7a8
Saravanakumar <Saravanakumar@Saravana>
2017-04-06 22:41:59 +0530

    trying multiple type of model and view objects

--- src/main/java/com/saravana/mavenwebapp/controller/HelloWorldController.java
+++ src/main/java/com/saravana/mavenwebapp/controller/HelloWorldController.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
  
@@ -11,16 +13,32 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloWorldController {
  
 	
-	@RequestMapping("/hello")
+	/*@RequestMapping("/hello")
 	public ModelAndView hello() {
 	    ModelAndView mav = new ModelAndView();
 	    Map<String, String> message = new HashMap<String, String>();
-	    message.put("message", "Hello World");
+	    message.put("msg1", "Hello World");
 	    mav.setViewName("helloworld");
-	    mav.addObject("message", message);
+	    mav.addAllObjects(message);
 	    return mav;
-	}
+	}*/
 	
+	/*@RequestMapping("hello")
+	public ModelAndView hello() {
+	ModelAndView model = new ModelAndView("helloworld");
+	model.addObject("msg", "hello world");
+
+	return model;
+	}*/
+	
+	
+	@RequestMapping("/hello")
+	public String hello(ModelMap model) {
+	    
+		model.addAttribute("msg", "Hello worle buddy");
+		
+		return "helloworld";
+	}
 	
  
 }
