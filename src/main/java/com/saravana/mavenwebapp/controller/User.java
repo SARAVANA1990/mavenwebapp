d95638b26620ef763367ee32c37c859386ccf852
saravanakumar <saravanakumar@.ncssoft.in>
2018-03-21 21:33:27 +0530

    file added

---
+++ src/main/java/com/saravana/mavenwebapp/controller/User.java
@@ -0,0 +1,36 @@
+package com.saravana.mavenwebapp.controller;
+
+public class User {
+	private String myMsg;
+	
+	
+
+	public String getMyMsg() {
+		return myMsg;
+	}
+
+	public void setMyMsg(String myMsg) {
+		this.myMsg = myMsg;
+	}
+
+	private String name;
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
